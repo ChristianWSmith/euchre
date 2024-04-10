fb1477f0e57fe77a588bfa59a0371bec82332f99
Christian Smith <smith.christian.william@gmail.com>
2024-04-10 17:43:55 -0400

    move input outline to enums.rs

--- src/main.rs
+++ src/main.rs
@@ -8,42 +8,6 @@ mod neural_network;
 use neural_network::*;
 
 fn main() {
-    // Game (20)
-    // self team score           - 10 indices (0-9 points)
-    // opponent team score       - 10 indices (0-9 points)
-
-    // Round (62)
-    // dealer                    - 4 indices  (1 for each player, left/ally/right)
-    // self team trick count     - 5 indices  (0-4 tricks)
-    // opponent team trick count - 5 indices  (0-4 tricks)
-    // upcard                    - 24 indices (1 for each card)
-    // hand                      - 24 indices (1 for each card)
-
-    // Bid Upcard (12)
-    // self action               - 3 indices  (make, make alone, pass)
-    // left opponent action      - 3 indices  (make, make alone, pass)
-    // ally action               - 3 indices  (make, make alone, pass)
-    // right opponent action     - 3 indices  (make, make alone, pass)
-
-    // Bid Suit (36)
-    // self action               - 9 indices  (4 makes, 4 make alones, pass)
-    // left opponent action      - 9 indices  (4 makes, 4 make alones, pass)
-    // ally action               - 9 indices  (4 makes, 4 make alones, pass)
-    // right opponent action     - 9 indices  (4 makes, 4 make alones, pass)
-
-    // Play (4)
-    // trump suit                - 4 indices  (4 suits)
-
-    // Trick (105 * 4 = 420)
-    // lead player               - 4 indices (self/left/ally/right)
-    // lead suit                 - 5 indices (4 suits, 1 "not set")
-    // first card                - 24 indices (1 for each card)
-    // second card               - 24 indices (1 for each card)
-    // third card                - 24 indices (1 for each card)
-    // fourth card               - 24 indices (1 for each card)
-
-    // Total (554)
-
     // number of max simultaneously extant networks times 2
     const NUM_NETWORKS: usize = 4;
     let stack_size: usize = mem::size_of::<NeuralNetwork>() * NUM_NETWORKS * 2;
